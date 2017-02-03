01a52fd39c4cd9ee3fab66f286b78f334d8cc6da
e2dubba <svirgilgoodwin@gmail.com>
2017-02-03 09:19:09 -0600

    added urls to config

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-var querry;
 var jsonurl = {
     "de" : "https://dict.leo.org/ende/index_de.html#/search={{s}}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on",
     "nl" : "http://www.vandale.nl/opzoeken?pattern={{s}}&lang=ne",
@@ -10,7 +9,13 @@ var jsonurl = {
     "ru" : "http://www.multitran.ru/c/M.exe?l1=1&l2=2&s=+{{s}}",
     "ar" : "http://www.egyptianarabicdictionary.com/online/search.php?ui=en&language=EG&key={{s}}&action=s",
     "wkt" : "https://en.wiktionary.org/wiki/{{s}}",
-    "log" : "http://logeion.uchicago.edu/index.html#{{s}}"
+    "log" : "http://logeion.uchicago.edu/index.html#{{s}}",
+    "el" : "http://logeion.uchicago.edu/index.html#{{s}}",
+    "la" : "http://logeion.uchicago.edu/index.html#{{s}}",
+    "persg" : "http://www.perseus.tufts.edu/hopper/morph?l={{s}}&la=greek",
+    "persl" : "http://www.perseus.tufts.edu/hopper/morph?l={{s}}&la=la",
+    "lccid" : "http://id.loc.gov/search/?q={{s}}&q=",
+    "viaf" : "http://viaf.org/viaf/search?query=local.names+all+\"{{s}}\"&sortKeys=holdingscount&recordSchema=BriefVIAF"
 };
 
 /*
